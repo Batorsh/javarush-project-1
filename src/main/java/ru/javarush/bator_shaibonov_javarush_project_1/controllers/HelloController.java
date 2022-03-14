42e1b16119cfbabcbc07b768f2dcc1b4b4d8b57f
Bator <b.shaibonov@gmail.com>
2022-03-14 23:26:13 +0300

    Initial Commit

--- src/main/java/ru/javarush/bator_shaibonov_javarush_project_1/controllers/HelloController.java
+++ src/main/java/ru/javarush/bator_shaibonov_javarush_project_1/controllers/HelloController.java
@@ -1,9 +1,11 @@
 package ru.javarush.bator_shaibonov_javarush_project_1.controllers;
 
 
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class HelloController {
 
@@ -14,8 +16,21 @@ public class HelloController {
     private URL location;
 
     @FXML
+    private Button cryptoButton;
+
+    @FXML
+    private Button uncryptoBruteForceButton;
+
+    @FXML
+    private Button uncryptoButton;
+
+    @FXML
+    private Button uncryptoStaticMethodButton;
+
+    @FXML
     void initialize() {
 
     }
 
 }
+

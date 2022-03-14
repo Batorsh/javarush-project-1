00581633d74564ff0403ee0878086b76d4d04728
Bator <b.shaibonov@gmail.com>
2022-03-15 00:12:57 +0300

    Initial Commit

---
+++ src/main/java/ru/javarush/bator_shaibonov_javarush_project_1/controllers/CryptoController.java
@@ -0,0 +1,41 @@
+package ru.javarush.bator_shaibonov_javarush_project_1.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CryptoController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField cryptoKey;
+
+    @FXML
+    private TextField readFileField;
+
+    @FXML
+    private Button uncryptoStaticMethodButton;
+
+    @FXML
+    private TextField writeFileField;
+
+    @FXML
+    void startCryptoButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
+
--- src/main/java/ru/javarush/bator_shaibonov_javarush_project_1/controllers/HelloController.java
+++ src/main/java/ru/javarush/bator_shaibonov_javarush_project_1/controllers/HelloController.java
@@ -1,11 +1,16 @@
 package ru.javarush.bator_shaibonov_javarush_project_1.controllers;
 
 
-
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import ru.javarush.bator_shaibonov_javarush_project_1.HelloApplication;
 
 public class HelloController {
 
@@ -29,7 +34,21 @@ public class HelloController {
 
     @FXML
     void initialize() {
+        cryptoButton.setOnAction(event -> {
+            cryptoButton.getScene().getWindow().hide();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("crypto-view.fxml"));
+            //fxmlLoader.setLocation(getClass().getResource("crypto-view.fxml"));
 
+            try {
+                fxmlLoader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Parent root = fxmlLoader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
     }
 
 }

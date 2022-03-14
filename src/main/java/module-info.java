c5300390ace820d5ae731348405e7fe1c841d2f9
Bator <b.shaibonov@gmail.com>
2022-03-14 23:07:45 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module ru.javarush.bator_shaibonov_javarush_project_1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens ru.javarush.bator_shaibonov_javarush_project_1 to javafx.fxml;
+    exports ru.javarush.bator_shaibonov_javarush_project_1;
+}
---
+++ src/main/java/ru/javarush/bator_shaibonov_javarush_project_1/HelloController.java
@@ -0,0 +1,14 @@
+package ru.javarush.bator_shaibonov_javarush_project_1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}

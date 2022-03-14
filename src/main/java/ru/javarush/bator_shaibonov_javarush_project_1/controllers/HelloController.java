d66dbbcf93f46ee8da882a7d629e41672fc6d363
Bator <b.shaibonov@gmail.com>
2022-03-14 23:16:01 +0300

    Initial Commit

---
+++ src/main/java/ru/javarush/bator_shaibonov_javarush_project_1/controllers/HelloController.java
@@ -0,0 +1,21 @@
+package ru.javarush.bator_shaibonov_javarush_project_1.controllers;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class HelloController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}

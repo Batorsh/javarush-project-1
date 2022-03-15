3279d356b980f003321526c307ad807845982300
Bator <b.shaibonov@gmail.com>
2022-03-15 10:12:32 +0300

    Made crypto-view.fxml and CheckInputData Made checking of inputdata in CryptoController

--- src/main/java/ru/javarush/bator_shaibonov_javarush_project_1/controllers/CryptoController.java
+++ src/main/java/ru/javarush/bator_shaibonov_javarush_project_1/controllers/CryptoController.java
@@ -2,10 +2,11 @@ package ru.javarush.bator_shaibonov_javarush_project_1.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import ru.javarush.bator_shaibonov_javarush_project_1.consoleui.CheckInputData;
 
 public class CryptoController {
 
@@ -22,20 +23,27 @@ public class CryptoController {
     private TextField readFileField;
 
     @FXML
-    private Button uncryptoStaticMethodButton;
+    private Button startCryptoButton;
 
     @FXML
     private TextField writeFileField;
 
     @FXML
-    void startCryptoButton(ActionEvent event) {
-
-    }
-
-    @FXML
     void initialize() {
-
+        startCryptoButton.setOnAction(event -> {
+            System.out.println("Чокибоки");
+            int typeOfOperation = 1;
+            String addressOfFileForRead = readFileField.getText().trim();
+            String addressOfFileForWrite = writeFileField.getText().trim();
+            String cypherKey = cryptoKey.getText().trim();
+            if (CheckInputData.isValidAddress(addressOfFileForRead, true, false) &&
+                    CheckInputData.isValidAddress(addressOfFileForWrite, true, true) &&
+            CheckInputData.isValidKey(cypherKey)) {
+                System.out.println("Введенные данные валидны: " + addressOfFileForRead + ", " + addressOfFileForWrite + ", " + cypherKey);
+            }
+        });
     }
 
 }
+
 
